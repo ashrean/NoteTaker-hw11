fcbc51c53b56466a23101fe767dc875ad9df97c3
Ashley Sese <sese.ashrean@gmail.com>
2023-01-30 16:44:17 -0800

    created routes

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-// Interacts with the frontend 
+// Interacts with the frontend
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
+const app = express();
+
+const PORT = process.env.PORT || 3002
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
